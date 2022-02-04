b945135303d5f9ca76040900cd1368b1641d601c
ammuarjun123 <arjunmca14@gmail.com>
2022-02-04 17:36:32 +0530

    commit

---
+++ EmployeeManagement/Dockerfile
@@ -0,0 +1,7 @@
+From java:8
+
+EXPOSE 8080
+
+ADD taget/docke.jar docker.jar
+
+ENTRYPOINT [ "java","-jar","docker.jar" ]
---
+++ EmployeeManagement/src/test/java/com/tutorial/root/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
